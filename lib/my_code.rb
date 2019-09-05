88ed28564c5022f498a68e44fa4f03e2da0447ff
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-05 17:44:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -62,13 +62,9 @@ end
 # REDUCE METHODS
 
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
 i = 0
-total = 0 
-
-  if starting_point 
-    total = starting_point
-  end
+total = starting_point 
 
   while i < source_array.length do 
     new_value = source_array[i]

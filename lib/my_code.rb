9051d08f2db65d07a602374d2f7d912af1791f8b
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-05 17:34:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -85,6 +85,7 @@ def reduce_to_all_true(source_array)
   while i < source_array.length do
     if source_array[i] == true 
       answer = true
+      return true
     else
       answer = false
       return answer

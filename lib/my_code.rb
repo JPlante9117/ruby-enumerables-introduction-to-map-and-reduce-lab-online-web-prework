6938b402352c5bdba986243b2c301ed717db768b
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-05 17:09:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,81 @@
-# My Code here....
+# MAP METHODS
+
+def map_to_negativize(source_array)
+i = 0 
+negative_array = []
+  
+  while i < source_array.length do
+    new_value = source_array[i] * -1
+    negative_array << new_value
+    
+    i += 1
+  end
+negative_array
+end
+
+def map_to_no_change(source_array)
+i = 0 
+same_array = []
+
+  while i < source_array.length do
+
+    new_value = source_array[i]
+    same_array.push(new_value)
+
+i += 1
+  end
+  
+  same_array
+end
+
+def map_to_double(source_array)
+  i = 0 
+  doubled_array = []
+  
+  while i < source_array.length do
+    
+    new_value = source_array[i] * 2 
+    doubled_array.push(new_value)
+    
+    i += 1
+  end
+  
+  doubled_array
+end
+
+def map_to_square(source_array)
+  i = 0 
+  squared_array = []
+  
+  while i < source_array.length do
+    
+    new_value = source_array[i] ** 2 
+    squared_array.push(new_value)
+     i += 1
+  end
+  
+  squared_array
+end
+
+
+
+# REDUCE METHODS
+
+
+def reduce_to_total(source_array, starting_point)
+i = 0
+total = 0
+
+if starting_point
+  total = starting_point
+end
+
+  while i < source_array.length do 
+    new_value = source_array[i]
+    total = total + new_value
+    
+    i += 1
+  end
+ total
+end  
+
